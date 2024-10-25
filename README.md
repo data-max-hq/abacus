da4cc05bc2033c25848a3ecf241a2812cbaab09a
dardanx <dardan@data-max.io>
2024-10-25 08:17:13 +0200

    Upload to s3 via cicd

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 ```
 poetry run pre-commit run --all-files
 ```
-  
+
+# Upload script
+aws s3 cp main.py s3://abacus-internal-glue-artefacts/main.py
+
+# Upload package
+aws s3 cp dist/*.whl s3://abacus-internal-glue-artefacts
+
+# Submit a new glue job
+
+aws glue update-job --job-name abacus-job --job-update "{\"Command\": {\"Name\": \"glueetl\", \"PythonVersion\": \"3\", \"ScriptLocation\": \"s3://abacus-internal-glue-artefacts/main.py\"}, \"DefaultArguments\": {\"--extra-py-files\": \"s3://abacus-internal-glue-artefacts/src-0.1.0-py3-none-any.whl\"}}"
+aws glue update-job --job-name abacus-job --job-update "{\"Role\": \"arn:aws:iam::925061584404:role/glue-service-account-role\", \"Command\": {\"Name\": \"glueetl\", \"ScriptLocation\": \"s3://abacus-internal-glue-artefacts/main.py\"}, \"DefaultArguments\": {\"--extra-py-files\": \"s3://abacus-internal-glue-artefacts/src-0.1.0-py3-none-any.whl\"}}"
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-
-from db_connection_util import get_spark_session
-
-from src.insert_atmp_t17.calculate_atmp_cc_formulas import calculate_atmp_cc_formulas
-from src.insert_atmp_t17.check_customer_product import check_customer_product
-from src.insert_atmp_t17.check_product_type import check_product_type
-from src.insert_atmp_t17.commit_atmp17_into_t17 import commit_atmpt17_into_t17
-from src.insert_atmp_t17.commit_atmp18_into_t18 import commit_atmpt18_into_t18
-from src.insert_atmp_t17.load_prev_atmp_t17 import load_prev_atmp_t17
 
 
 def main():
@@ -40,11 +30,12 @@ def main():
 
 
 def dummy():
-    spark = SparkSession.builder \
-        .appName("SimplePySparkExample") \
-        .config("spark.driver.host", "localhost") \
-        .config("spark.driver.port", "0") \
+    spark = (
+        SparkSession.builder.appName("SimplePySparkExample")
+        .config("spark.driver.host", "localhost")
+        .config("spark.driver.port", "0")
         .getOrCreate()
+    )
 
     # Create dummy data
     data = [
@@ -55,7 +46,7 @@ def dummy():
         ("Eva", 39, "Engineering", 80000),
         ("Frank", 45, "Marketing", 65000),
         ("Grace", 33, "Sales", 52000),
-        ("Henry", 37, "Engineering", 78000)
+        ("Henry", 37, "Engineering", 78000),
     ]
 
     # Define the schema
@@ -67,10 +58,6 @@ def dummy():
     # Show the DataFrame
     print("Original DataFrame:")
     df.show()
-
-    # Perform some operations
-    # 1. Filter employees older than 35
-    older_employees = df.filter(col("age") > 35)
 
 
 if __name__ == "__main__":
